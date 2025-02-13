e0358b17b62ccffe645232fbe01b415b55a26337
Vinicius Cechin <vinicius.cechin@datathings.com>
2025-02-13 10:24:56 +0100

    exercise 1.7

--- part_1/unicafe/src/App.jsx
+++ part_1/unicafe/src/App.jsx
@@ -11,13 +11,27 @@ function Grade(props) {
 }
 
 function App() {
-  const grades = ["good", "neutral", "bad"];
+  const grades = ["Good", "Neutral", "Bad"];
   const [values, setValues] = useState([0, 0, 0]);
 
   function handleOnGradeClick(index) {
     const newValues = [...values];
     newValues[index] += 1;
     setValues(newValues);
+  }
+
+  function buildGradeStatistics() {
+    const count = values.reduce((res, value) => {
+      return res + value;
+    });
+    const divider = (count > 0 ? count : 1);
+    return (
+      <div>
+        <p>All: {count}</p>
+        <p>Average: {(values[0] - values[2]) / divider}</p>
+        <p>Positive: {(values[0] / divider) * 100}%</p>
+      </div>
+    );
   }
 
   return (
@@ -35,6 +49,7 @@ function App() {
         {grades.map((grade, idx) => (
           <Grade key={idx} grade={grade} value={values[idx]} />
         ))}
+        {buildGradeStatistics()}
       </div>
     </div>
   )
