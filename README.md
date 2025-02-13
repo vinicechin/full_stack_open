960bfaf7881d995d82f9dc375cc95d488440ba7c
Vinicius Cechin <vinicius.cechin@datathings.com>
2025-02-13 13:15:58 +0100

    exercise 1.1

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Full stack open course exercises
 
 part 1:
 1h - 12/02
+13/02 - 9:30 to 11:30 + 30min
---
+++ part_1/introdemo/src/App.jsx
@@ -0,0 +1,51 @@
+function Header(props) {
+  return (
+    <h1>{props.text}</h1>
+  );
+}
+
+function Content(props) {
+  return (
+    <>
+      {props.parts.map((part, idx) => {
+        return (
+          <p key={idx}>
+            {part.label} {part.value}
+          </p>
+        )
+      })}
+    </>
+  );
+}
+
+function Total(props) {
+  return (
+    <p>Number of exercises {props.value}</p>
+  );
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header text={course} />
+      <Content
+        parts={[
+          { label: part1, value: exercises1 },
+          { label: part2, value: exercises2 },
+          { label: part3, value: exercises3 },
+        ]}
+      />
+      <Total value={exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+}
+
+export default App
