f2d4c2e9b5fe610b4866b1891176b886221239d7
Vinicius Cechin <vinicius.cechin@datathings.com>
2025-05-21 11:19:33 +0200

    exercise 3.3

--- part_3/phonebook/index.js
+++ part_3/phonebook/index.js
@@ -35,6 +35,17 @@ app.get('/api/info', (_, response) => {
   response.end(`<p>Phonebook has info for ${count} people</p><p>${timestamp.toString()}</p>`);
 });
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id;
+  const person = persons.find(person => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
