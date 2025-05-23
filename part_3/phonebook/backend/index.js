00e5ed2e7e0849e0968c5730e8699cd03950f5bc
Vinicius Cechin <vinicius.cechin@datathings.com>
2025-05-23 18:24:59 +0200

    exercise 3.17

--- part_3/phonebook/backend/index.js
+++ part_3/phonebook/backend/index.js
@@ -47,6 +47,25 @@ app.delete('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error));
 });
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body;
+
+  Person.findById(request.params.id)
+    .then(person => {
+      if (!person) {
+        return response.status(404).end()
+      }
+
+      person.name = name;
+      person.number = number;
+
+      return person.save().then((updatedPerson) => {
+        response.json(updatedPerson)
+      })
+    })
+    .catch(error => next(error))
+})
+
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
