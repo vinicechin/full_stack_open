8408a831cfd83bb904fea8d3a2f04be78fe512a3
Vinicius Cechin <vinicius.cechin@datathings.com>
2025-05-30 12:14:20 +0200

    exercise 4.4

--- part_4/blog/tests/dummy.test.js
+++ part_4/blog/tests/dummy.test.js
@@ -1,4 +1,4 @@
-const { test } = require("node:test");
+const { test, describe } = require("node:test");
 const assert = require("node:assert");
 const listHelper = require("../utils/list_helper");
 
@@ -7,4 +7,45 @@ test("dummy returns one", () => {
 
   const result = listHelper.dummy(blogs);
   assert.strictEqual(result, 1);
+});
+
+describe("total likes", () => {
+  const blogs = [
+    {
+      _id: '5a422aa71b54a676234d17f6',
+      title: 'Test 1',
+      author: 'Test User',
+      url: 'https://test_1.com',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f7',
+      title: 'Test 2',
+      author: 'Test User',
+      url: 'https://test_2.com',
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Test 3',
+      author: 'Test User',
+      url: 'https://test_3.com',
+      likes: 3,
+      __v: 0
+    },
+  ];
+
+  test("empty list", () => {
+    assert.strictEqual(listHelper.totalLikes([]), 0);
+  });
+
+  test("1 element list", () => {
+    assert.strictEqual(listHelper.totalLikes([blogs[0]]), 5);
+  });
+
+  test("More than 1 element list", () => {
+    assert.strictEqual(listHelper.totalLikes(blogs), 15);
+  });
 });
--- part_4/blog/utils/list_helper.js
+++ part_4/blog/utils/list_helper.js
@@ -1,7 +1,12 @@
-const dummy = (blogs) => {
+function dummy(blogs) {
   return blogs ? 1 : 1;
 };
 
+function totalLikes(blogs) {
+  return blogs.reduce((s, v) => s + v.likes, 0);
+}
+
 module.exports = {
   dummy,
+  totalLikes,
 };
